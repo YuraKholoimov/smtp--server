b1305a9fba5eff374af83924cd1b7d9325fdeb97
Yura Kholoimov <yurakholoimov@ya.ru>
2022-09-10 22:23:00 +0300

    Add env variables

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(cors());
-app.options('*', cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 let smtp_login = process.env.SMPT_LOGIN || "---";
 let smtp_pass = process.env.SMTP_PASS || "---";
