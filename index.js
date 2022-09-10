90d367158d0c9d1a9300ba81f8eed17aeb9c8454
Yura Kholoimov <yurakholoimov@ya.ru>
2022-09-10 20:33:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'yuriykholoimov@gmail.com', // generated ethereal user
+        pass: 'lsujotmrydifywye' // generated ethereal password
+    },
+});
+
+app.get('/', (req, res) => {
+    res.send({message: 'Hello WWW!'});
+});
+
+app.post('/sendMail', async (req, res) => {
+    const {name, email, phone, message} = req.body
+
+    let info = await transporter.sendMail({
+        from: `${email}`, // sender address
+        to: "yuriykholoimov@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        // text: "Hello world?", // plain text body
+        html: `<b>Сообщение с сайта Портфолио</b>
+<div>
+name: ${name}, email: ${email}, phone: ${phone}
+</div>
+<div>
+Message: ${message}
+</div>
+`, // html body
+    });
+
+    res.send({message: req.body});
+});
+
+app.listen(3001, () => {
+    console.log('Application listening on port 3001!');
+});
+
