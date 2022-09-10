587b331e13cfe5aabb93a984baa683dfe8482aee
Yura Kholoimov <yurakholoimov@ya.ru>
2022-09-10 22:27:09 +0300

    Add env variables

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use(function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
